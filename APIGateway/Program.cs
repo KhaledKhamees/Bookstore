d8a8a736e766e9c22cd7c542628fe190cee32198
khaled khamees <khaledkhameesabdelaleem@gmail.com>
2025-08-21 23:54:35 +0300

    Adding an API Gateway.

---
+++ APIGateway/Program.cs
@@ -0,0 +1,53 @@
+
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using System.Threading.Tasks;
+
+namespace APIGateway
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+            builder.Services.AddOcelot(builder.Configuration);
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllOrigins",
+                    builder => builder.AllowAnyOrigin()
+                                      .AllowAnyMethod()
+                                      .AllowAnyHeader());
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseCors("AllowAllOrigins");
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+            
+            await app.UseOcelot();
+
+            app.Run();
+        }
+    }
+}
