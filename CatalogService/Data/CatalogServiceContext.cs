8e76f895994dcd1127baf91a3582e543eab58ad5
khaled khamees <khaledkhameesabdelaleem@gmail.com>
2025-08-21 18:04:03 +0300

    Add CatalogService

---
+++ CatalogService/Data/CatalogServiceContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CatalogService.Models;
+
+namespace CatalogService.Data
+{
+    public class CatalogServiceContext : DbContext
+    {
+        public CatalogServiceContext (DbContextOptions<CatalogServiceContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CatalogService.Models.Book> Book { get; set; } = default!;
+    }
+}
