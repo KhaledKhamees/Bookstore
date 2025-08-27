2e3d2186782b2e22b81daa9151430832a6e46f38
khaled khamees <khaledkhameesabdelaleem@gmail.com>
2025-08-27 12:08:46 +0300

    UserService - latest version improved

--- UserService/Data/UserDbContext.cs
+++ UserService/Data/UserDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using UserService.Models;
 
 namespace UserService.Data
 {
-    public class UserDbContext : IdentityDbContext<Models.ApplicationUser>
+    public class UserDbContext : IdentityDbContext<ApplicationUser,ApplicationRole,string>
     {
         public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
         {
